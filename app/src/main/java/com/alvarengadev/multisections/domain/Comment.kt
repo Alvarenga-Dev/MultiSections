fcb96fc3c3d8a4048938995f97aa10d4a81b4bc9
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-07-21 09:32:20 -0300

    create extensions; refactor Comment.kt

--- app/src/main/java/com/alvarengadev/multisections/domain/Comment.kt
+++ app/src/main/java/com/alvarengadev/multisections/domain/Comment.kt
@@ -1,13 +1,15 @@
 package com.alvarengadev.multisections.domain
 
 data class Comment(
-    private val userName: String,
-    private val comment: String,
-    private val resPhotoProfile: Int,
-    private val isLike: Boolean,
-    private var repliesList: ArrayList<Comment>? = null,
+    val userName: String,
+    val userComment: String,
+    val resPhotoProfile: Int,
+    val isLike: Boolean,
+    private var repliesList: ArrayList<Comment>? = null
 ) {
     fun setRepliesList(repliesList: ArrayList<Comment>) {
         this.repliesList = repliesList
     }
+
+    fun getRepliesList() = this.repliesList
 }
---
+++ app/src/main/java/com/alvarengadev/multisections/utils/extensions/TextViewExtensions.kt
@@ -0,0 +1,7 @@
+package com.alvarengadev.multisections.utils.extensions
+
+import android.widget.TextView
+
+fun TextView.text(text: String) {
+    this.text = text
+}
